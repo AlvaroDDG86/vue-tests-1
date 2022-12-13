67d30b008458f638ffb4d392d3029cd7cc9977f2
Álvaro de Dios <alvarodediosgarcia1986@gmail.com>
2022-12-13 18:27:46 +0100

    list component shallowMount

--- specs/test.spec.js
+++ specs/test.spec.js
@@ -1,7 +1,17 @@
-import { mount } from '@vue/test-utils'
+import { mount, shallowMount } from '@vue/test-utils'
 
 import TestComponent from '@/test.vue'
-test('Mount the component', () => {
-    const wrapper = mount(TestComponent)
-    console.log(wrapper)
+import List from '@/list.vue'
+test('Mount the test component', () => {
+    const wrapper = mount(TestComponent, {
+        propsData: {
+            value: 'Test'
+        }
+    })
+    expect(wrapper.html()).toMatchSnapshot()
+})
+
+test('Mount the list component', () => {
+    const wrapper = shallowMount(List)
+    expect(wrapper.html()).toMatchSnapshot()
 })
